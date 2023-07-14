343ea67d3c9240303d57b2669bfb76f58a4cefa2
coelhoreinaldo <scoelhor7@gmail.com>
2023-07-14 10:44:26 -0300

    :sparkles: feat: req 11, postcategory model

---
+++ src/models/PostCategory.js
@@ -0,0 +1,50 @@
+const PostCategorySchema = (sequelize, DataTypes) => {
+  const PostCategoryTable = sequelize.define('PostCategory', {
+    postId: {
+      type: DataTypes.INTEGER,
+      field: 'post_id',
+      references: {
+        model: 'blog_posts',
+        key: 'id',
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
+      primaryKey: true,
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      field: 'category_id',
+      references: {
+        model: 'categories',
+        key: 'id',
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
+      primaryKey: true,
+    },
+  },
+    {
+      timestamps: false,
+      underscored: true,
+      tableName: 'post_categories'
+    },
+  );
+
+  PostCategoryTable.associate = ({ Category, BlogPost }) => {
+    BlogPost.belongsToMany(Category, {
+      as: 'categories',
+      through: PostCategoryTable,
+      foreignKey: 'postId',
+      otherKey: 'categoryId'
+    })
+    Category.belongsToMany(BlogPost, {
+      as: 'blog_posts',
+      through: PostCategoryTable,
+      foreignKey: 'categoryId',
+      otherKey: 'postId'
+    })
+  }
+  return PostCategoryTable;
+}
+
+module.exports = PostCategorySchema
